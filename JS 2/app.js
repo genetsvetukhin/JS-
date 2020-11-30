73746abcaf68742e27efc774b57d203859c26c76
Gene <e.tsvetukhin@gmail.com>
2020-12-01 01:45:05 +0300

    main

---
+++ JS 2/app.js
@@ -0,0 +1,10 @@
+const user = 'Евгений';
+let age = 29;
+
+
+console.log(typeof user, typeof age );
+
+function helloWorld() {
+    console.log('Привет, ' + user + '!');
+}
+helloWorld();
---
+++ JS 3_swiperLanding/script.js
@@ -0,0 +1,53 @@
+
+let pageSlider = new Swiper('.page',{
+    // Мои классы
+    wrapperClass: 'page__wrapper',
+    slideClass: 'page__screen',
+    
+    // Вертикальный слайдер
+    direction: 'vertical',
+
+    // Кол-во слайдов для показа
+    sliderPerView: 'auto',
+
+    // Вклчаем параллакс
+    parallax: true,
+
+    // Управление клавиатурой
+    keyboard: {
+        // вкл-выкл
+        enabled: true,
+        onlyInViewport: true,
+        // Управление стрелками
+        pageUpDown: true,
+    },
+    
+    // Упр. колесом мыши
+    mousewheel: {
+        sensitivity: 1,
+    },
+
+    watchOverflow: true,
+    speed: 800,
+    observer: true,
+    observeParents: true,
+    observeSlideChildren: true,
+
+    // Навигация
+    // Буллеты, текущее положение, прогрессбар
+    pagination: {
+        el: ".page__pagination",
+        type: 'bullets',
+        clickable: true,
+        bulletClass: 'page__bullet',
+        bulletActiveClass: 'page__bullet_active'
+    },
+
+    //Скролл
+    scrollbar: {
+        el: ".page__scroll",
+        dragClass: 'page__drag-scroll',
+        //Возможность перетаскивать скролл
+        draggable: true,
+    },
+}) 
